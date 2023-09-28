e73f70fff6679627afddfc2fc1be137502fe3239
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2023-09-28 09:23:33 -0700

    Se esconde la lista de asesores

--- resources/views/website/members.blade.php
+++ resources/views/website/members.blade.php
@@ -42,7 +42,7 @@
 
                 <!-- Biografias del documento enviado por Cynthia Landa -->
 
-                <h2 class="text-primary mb-10">
+                <h2 class="text-center md:text-left text-primary mb-10">
                     Socios Fundadores
                 </h2>
 
@@ -103,7 +103,7 @@
                     </x-team-member-card>
                 </div>
 
-                <h2 class="text-primary mb-10">
+                <h2 class="text-center md:text-left text-primary mb-10">
                     Colaboradores
                 </h2>
 
@@ -142,82 +142,84 @@
                     </x-team-member-card>
                 </div>
 
-                <h2 class="text-primary mb-10">
-                    Asesores
-                </h2>
+                {{-- No se muestran los asesores pero se dejaran aqui por si es requerido ponerlos nuevamente
+                    <h2 class="text-center md:text-left text-primary mb-10">
+                        Asesores
+                    </h2>
 
-                <div class="w-full mb-10">
-                    <x-team-member-card 
-                        name="Alberto Godínez Placencia"
-                        jobtitle="Asesor"
-                        img="CELUK_AlbertoGodinez.png"
-                        gender="male">
-                        Doctor en Economía Agrícola por El Colegio de Posgraduados (COLPOS), maestro en Desarrollo Regional por el Colegio de la Frontera Norte (COLEF), e Ingeniero Agrónomo con especialidad en Economía Agrícola por la Universidad Autónoma Chapingo (UACH). <br><br>
-                        Cuenta con amplia experiencia en investigación económica cuantitativa aplicando los conceptos y herramientas de Economía Matemática, Matemática Financiera, Econometría y Estadística Matemática. En su carrera se ha especializado en la formulación, implementación, seguimiento, evaluación y mejora de Planes Estratégicos, Programas Operativo-Financieros anuales, así como en la formulación y evaluación de Proyectos de inversión empresarial, tanto en el sector privado, instituciones científico-académicas, agencias gubernamentales y organizaciones no gubernamentales. <br><br>
-                        Se ha desempeñado como Agribusiness Intelligence Manager en Gebyacc Agrícola, como director de Planificación Estratégica y Monitoreo de la Inversión Pública en la Secretaría de Desarrollo Agropecuario del Gobierno del Estado de Baja California, y como director académico en Fundación Rafael Preciado Hernández, A.C. También ha sido profesor-investigar en la Universidad Autónoma de Baja California y en El Colegio de la Frontera Norte.
-                    </x-team-member-card>
-                </div>
+                    <div class="w-full mb-10">
+                        <x-team-member-card 
+                            name="Alberto Godínez Placencia"
+                            jobtitle="Asesor"
+                            img="CELUK_AlbertoGodinez.png"
+                            gender="male">
+                            Doctor en Economía Agrícola por El Colegio de Posgraduados (COLPOS), maestro en Desarrollo Regional por el Colegio de la Frontera Norte (COLEF), e Ingeniero Agrónomo con especialidad en Economía Agrícola por la Universidad Autónoma Chapingo (UACH). <br><br>
+                            Cuenta con amplia experiencia en investigación económica cuantitativa aplicando los conceptos y herramientas de Economía Matemática, Matemática Financiera, Econometría y Estadística Matemática. En su carrera se ha especializado en la formulación, implementación, seguimiento, evaluación y mejora de Planes Estratégicos, Programas Operativo-Financieros anuales, así como en la formulación y evaluación de Proyectos de inversión empresarial, tanto en el sector privado, instituciones científico-académicas, agencias gubernamentales y organizaciones no gubernamentales. <br><br>
+                            Se ha desempeñado como Agribusiness Intelligence Manager en Gebyacc Agrícola, como director de Planificación Estratégica y Monitoreo de la Inversión Pública en la Secretaría de Desarrollo Agropecuario del Gobierno del Estado de Baja California, y como director académico en Fundación Rafael Preciado Hernández, A.C. También ha sido profesor-investigar en la Universidad Autónoma de Baja California y en El Colegio de la Frontera Norte.
+                        </x-team-member-card>
+                    </div>
 
-                <div class="w-full mb-10">
-                    <x-team-member-card 
-                        name="Alfonso Camberos Urbina"
-                        jobtitle="Asesor"
-                        img="CELUK_AlfonsoCamberos.png"
-                        gender="male">
-                        
-                        Egresado de la Licenciatura en Arquitectura por el Instituto Tecnológico de Tijuana con especialidad en Diseño urbano y la Maestría en Administración Integral del Ambiente por el Colegio de la Frontera Norte (COLEF) y el Centro de Investigación Científica y Enseñanza Superior de Ensenada (CICESE).  Ha sido consultor asociado en diversos proyectos estudios y programas de desarrollo urbano y ambiental, programas de desarrollo urbano estatales y municipales, así como consultor asociado en planeación, análisis del paisaje, caracterización del ecosistema y Sistemas de Información Geográfica para la certificación de desarrollos urbanos integrales y sustentables.  Durante cinco años condujo la Jefatura del Departamento de Planeación Urbana y Ordenamiento Territorial en el Instituto de Planeación de Desarrollo Municipal en Tecate, Baja California. Actualmente es coordinador de la consultoría Land GIS design dedicada al análisis del paisaje y Sistemas de Información Geográfica.
-                        
-                    </x-team-member-card>
-                </div>
+                    <div class="w-full mb-10">
+                        <x-team-member-card 
+                            name="Alfonso Camberos Urbina"
+                            jobtitle="Asesor"
+                            img="CELUK_AlfonsoCamberos.png"
+                            gender="male">
+                            
+                            Egresado de la Licenciatura en Arquitectura por el Instituto Tecnológico de Tijuana con especialidad en Diseño urbano y la Maestría en Administración Integral del Ambiente por el Colegio de la Frontera Norte (COLEF) y el Centro de Investigación Científica y Enseñanza Superior de Ensenada (CICESE).  Ha sido consultor asociado en diversos proyectos estudios y programas de desarrollo urbano y ambiental, programas de desarrollo urbano estatales y municipales, así como consultor asociado en planeación, análisis del paisaje, caracterización del ecosistema y Sistemas de Información Geográfica para la certificación de desarrollos urbanos integrales y sustentables.  Durante cinco años condujo la Jefatura del Departamento de Planeación Urbana y Ordenamiento Territorial en el Instituto de Planeación de Desarrollo Municipal en Tecate, Baja California. Actualmente es coordinador de la consultoría Land GIS design dedicada al análisis del paisaje y Sistemas de Información Geográfica.
+                            
+                        </x-team-member-card>
+                    </div>
 
-                <div class="w-full mb-10">
-                    <x-team-member-card 
-                        name="Carlos Orozco Riezgo"
-                        jobtitle="Asesor"
-                        img="CELUK_CarlosOrozco.png"
-                        gender="male">
-                        
-                        Doctor en ciencias agrícolas con maestría en uso y gestión de agua de riego en regiones áridas. Es una autoridad en el uso de tecnologías innovadoras para la mejora de producción de cultivos como forrajes, aceitunas y trigo. Es pionero en la instalación de sistemas de drenaje agrícola subterráneos; en 1997 implementó un programa para instalar un sistema de drenaje subterráneo en el Valle de Mexicali. Ocupó el cargo de Subsecretario en la Secretaría de Agricultura y Ganadería de Baja California, y en dos ocasiones el cargo de director de Agricultura, así como director de Infraestructura de Irrigación Agrícola. En 2003 planificó y supervisó la instalación del “Sistema de Información en el Manejo del Agua de Riego de Baja California” (SIMARBC), para proveer de información actualizada sobre el clima a los agricultores. 
-                        
-                    </x-team-member-card>
-                </div>
+                    <div class="w-full mb-10">
+                        <x-team-member-card 
+                            name="Carlos Orozco Riezgo"
+                            jobtitle="Asesor"
+                            img="CELUK_CarlosOrozco.png"
+                            gender="male">
+                            
+                            Doctor en ciencias agrícolas con maestría en uso y gestión de agua de riego en regiones áridas. Es una autoridad en el uso de tecnologías innovadoras para la mejora de producción de cultivos como forrajes, aceitunas y trigo. Es pionero en la instalación de sistemas de drenaje agrícola subterráneos; en 1997 implementó un programa para instalar un sistema de drenaje subterráneo en el Valle de Mexicali. Ocupó el cargo de Subsecretario en la Secretaría de Agricultura y Ganadería de Baja California, y en dos ocasiones el cargo de director de Agricultura, así como director de Infraestructura de Irrigación Agrícola. En 2003 planificó y supervisó la instalación del “Sistema de Información en el Manejo del Agua de Riego de Baja California” (SIMARBC), para proveer de información actualizada sobre el clima a los agricultores. 
+                            
+                        </x-team-member-card>
+                    </div>
 
-                <div class="w-full mb-10">
-                    <x-team-member-card 
-                        name="Efraín Carlos Nieblas Ortiz"
-                        jobtitle="Asesor"
-                        img="CELUK_EfrainNiebla.png"
-                        gender="male">
-                        
-                        El Dr. Nieblas es Biólogo, maestro en Ingeniería de Sistemas y Doctor en Ciencias, por la Universidad Autónoma de Baja California, donde es investigador de tiempo completo adscrito al Instituto de Ingeniería de la Universidad Autónoma de Baja California. En el Sector Público, ha sido director de Auditoría Ambiental, director de  Gestión Ambiental y secretario de Protección al Ambiente del Gobierno del Estado de Baja California. También fungió como director de Protección al Ambiente del 23ro. Ayuntamiento de Mexicali. Actualmente se desempeña como director Operativo de Consorcio Ambiental S.C., firma de consultoría y asesoría en materia de cumplimiento ambiental, seguridad e higiene y protección civil. <br><br>
+                    <div class="w-full mb-10">
+                        <x-team-member-card 
+                            name="Efraín Carlos Nieblas Ortiz"
+                            jobtitle="Asesor"
+                            img="CELUK_EfrainNiebla.png"
+                            gender="male">
+                            
+                            El Dr. Nieblas es Biólogo, maestro en Ingeniería de Sistemas y Doctor en Ciencias, por la Universidad Autónoma de Baja California, donde es investigador de tiempo completo adscrito al Instituto de Ingeniería de la Universidad Autónoma de Baja California. En el Sector Público, ha sido director de Auditoría Ambiental, director de  Gestión Ambiental y secretario de Protección al Ambiente del Gobierno del Estado de Baja California. También fungió como director de Protección al Ambiente del 23ro. Ayuntamiento de Mexicali. Actualmente se desempeña como director Operativo de Consorcio Ambiental S.C., firma de consultoría y asesoría en materia de cumplimiento ambiental, seguridad e higiene y protección civil. <br><br>
 
-                        Ha sido Consejero Honorario de Organismos de la Sociedad Civil dedicados a actividades de educación ambiental y de restauración ecológica, como: Fundación Hélice, Restauremos el Colorado y Proyecto Fronterizo de Educación Ambiental. En el ámbito gremial, ha sido presidente de la Asociación de Biólogos de Mexicali (BIOMAC); y fue presidente del Primer Consejo Directivo del Colegio de Biólogos de Baja California (CBBC) y ocupa en la actualidad la presidencia del Consejo Técnico Consultivo. En noviembre de 2022, el Dr. Nieblas recibió el Reconocimiento Nacional al Mérito Profesional en las Ciencias Biológicas, así como la Medalla Juan Luis Cifuentes Lemus, otorgados por la Federación Mexicana de Colegios de Biólogos (FEMCOBI).
-                        
-                    </x-team-member-card>
-                </div>
+                            Ha sido Consejero Honorario de Organismos de la Sociedad Civil dedicados a actividades de educación ambiental y de restauración ecológica, como: Fundación Hélice, Restauremos el Colorado y Proyecto Fronterizo de Educación Ambiental. En el ámbito gremial, ha sido presidente de la Asociación de Biólogos de Mexicali (BIOMAC); y fue presidente del Primer Consejo Directivo del Colegio de Biólogos de Baja California (CBBC) y ocupa en la actualidad la presidencia del Consejo Técnico Consultivo. En noviembre de 2022, el Dr. Nieblas recibió el Reconocimiento Nacional al Mérito Profesional en las Ciencias Biológicas, así como la Medalla Juan Luis Cifuentes Lemus, otorgados por la Federación Mexicana de Colegios de Biólogos (FEMCOBI).
+                            
+                        </x-team-member-card>
+                    </div>
 
-                <div class="w-full mb-10">
-                    <x-team-member-card 
-                        name="Joel García Campa"
-                        jobtitle="Asesor"
-                        gender="male">
-                        
-                        Es el director general de Estrategias SEGURAGUA, S.C., empresa líder en promover la autorregulación de los derechos y obligaciones de los concesionarios de aguas nacionales y permisionarios de descargas de aguas residuales en México. Asesor Jurídico de la Sección Mexicana de la Comisión Internacional de Límites y Aguas (CILA), tiene licenciatura en Derecho por la Universidad de Sonora y Maestría en Derecho Corporativo por la Universidad Anáhuac Campus Norte, Ciudad de México. Cuenta con más de 20 años de experiencia, ha sido consultor en materia de aguas nacionales para el gobierno federal, gobiernos Estatales y municipales. Laboró en la Comisión Nacional el Agua en la Ciudad de México en el área fiscal y posteriormente como director Jurídico en la Región Noroeste de la misma dependencia (1998-2010).
-                        
-                    </x-team-member-card>
-                </div>
+                    <div class="w-full mb-10">
+                        <x-team-member-card 
+                            name="Joel García Campa"
+                            jobtitle="Asesor"
+                            gender="male">
+                            
+                            Es el director general de Estrategias SEGURAGUA, S.C., empresa líder en promover la autorregulación de los derechos y obligaciones de los concesionarios de aguas nacionales y permisionarios de descargas de aguas residuales en México. Asesor Jurídico de la Sección Mexicana de la Comisión Internacional de Límites y Aguas (CILA), tiene licenciatura en Derecho por la Universidad de Sonora y Maestría en Derecho Corporativo por la Universidad Anáhuac Campus Norte, Ciudad de México. Cuenta con más de 20 años de experiencia, ha sido consultor en materia de aguas nacionales para el gobierno federal, gobiernos Estatales y municipales. Laboró en la Comisión Nacional el Agua en la Ciudad de México en el área fiscal y posteriormente como director Jurídico en la Región Noroeste de la misma dependencia (1998-2010).
+                            
+                        </x-team-member-card>
+                    </div>
 
-                <div class="w-full mb-10">
-                    <x-team-member-card 
-                        name="Juan Jaime Sánchez Meza"
-                        jobtitle="Asesor"
-                        img="CELUK_JaimeSanchez.png"
-                        gender="male">
-                        
-                        Es licenciado en Derecho egresado de la Universidad de Sonora. Cuenta además con estudios sobre Población y Desarrollo del Fondo de las Naciones Unidas para Actividades en Materia de Población. Es también especialista en Administración Pública Municipal del Instituto Sonorense de Administración Pública. En el Colegio de Sonora obtuvo una especialidad en la Gestión Integrada de Cuencas Hidrográficas. Posteriormente obtuvo un diplomado en Gobierno y Gestión Local por el Centro de Investigación y Docencia Económica (CIDE) y cursó el diplomado Dr. Jorge Carpizo en Derechos Humanos del Instituto de Investigaciones Jurídicas de la UNAM.
-                        
-                    </x-team-member-card>
-                </div>
+                    <div class="w-full mb-10">
+                        <x-team-member-card 
+                            name="Juan Jaime Sánchez Meza"
+                            jobtitle="Asesor"
+                            img="CELUK_JaimeSanchez.png"
+                            gender="male">
+                            
+                            Es licenciado en Derecho egresado de la Universidad de Sonora. Cuenta además con estudios sobre Población y Desarrollo del Fondo de las Naciones Unidas para Actividades en Materia de Población. Es también especialista en Administración Pública Municipal del Instituto Sonorense de Administración Pública. En el Colegio de Sonora obtuvo una especialidad en la Gestión Integrada de Cuencas Hidrográficas. Posteriormente obtuvo un diplomado en Gobierno y Gestión Local por el Centro de Investigación y Docencia Económica (CIDE) y cursó el diplomado Dr. Jorge Carpizo en Derechos Humanos del Instituto de Investigaciones Jurídicas de la UNAM.
+                            
+                        </x-team-member-card>
+                    </div>
+                --}}
 
                 <!-- Terminan biografias del documento enviado por Cynthia Landa -->
 
