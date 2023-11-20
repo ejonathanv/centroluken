2564ab6d0992bb466da032409d3ae2c5566770f6
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2023-11-19 21:13:07 -0800

    Traduccion de contacto

---
+++ lang/en/contact.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'title' => 'Contact us for more information',
+    'description' => 'Send us a message and we will contact you.',
+    'sending' => 'Sending...',
+    'name' => 'What is your full name?',
+    'email' => 'What is your email address?',
+    'phone' => 'What is your phone number?',
+    'message' => 'Write your message',
+    'button' => 'Send message',
+    'placeholder' => [
+        'name' => 'Write your full name',
+        'email' => 'Write your email address',
+        'phone' => 'Write your phone number',
+        'message' => 'Write your message'
+    ]
+];
