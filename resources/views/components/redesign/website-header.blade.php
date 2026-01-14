dffa191d46791fb647fdc2d5b20a7e747edccd8e
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2026-01-14 11:58:52 -0800

    Menu Movil - Fase 2 - Opciones

--- resources/views/components/redesign/website-header.blade.php
+++ resources/views/components/redesign/website-header.blade.php
@@ -6,6 +6,15 @@
     </div>
     <div class="container" x-data="{
         mobileMenu: false,
+        toggleMobileMenu() {
+            this.mobileMenu = !this.mobileMenu;
+
+            if (this.mobileMenu) {
+                document.body.style.overflow = 'hidden';
+            } else {
+                document.body.style.overflow = 'auto';
+            }
+        }
     }">
         <div class="flex items-center justify-between">
             <div class="w-2/3 lg:w-1/3">
@@ -76,7 +85,7 @@
                     <li>
                         <a href="#" class="text-white font-semibold text-sm hover:text-secondary relative"
                             style="z-index: 1001;"
-                            @click.prevent="mobileMenu = !mobileMenu">
+                            @click.prevent="toggleMobileMenu()">
                             <i x-show="!mobileMenu" class="lni lni-menu-hamburger-1 text-2xl md:text-3xl"></i>
                             <i x-show="mobileMenu" class="lni lni-xmark text-2xl md:text-3xl"></i>
                         </a>
@@ -86,7 +95,53 @@
         </div>
 
         <div class="mobile-menu" :class="{ 'active': mobileMenu }">
-            
+            <img src="{{ asset('img/centro-luken-logo-oscuro.svg') }}" 
+                alt="Centro Luken - De Estrategias en Agua y Medio Ambiente"
+                class="h-12 w-auto mb-12">
+
+            <nav class="mb-7">
+                <ul class="flex flex-col space-y-4">
+                    <li class="border-b-2 border-white/20 py-2">
+                        <a href="#" class="text-white font-semibold text-sm hover:text-secondary">
+                            Inicio
+                        </a>
+                    </li>
+                    <li class="border-b-2 border-white/20 py-2">
+                        <a href="#" class="text-white font-semibold text-sm hover:text-secondary">
+                            Filosofía
+                        </a>
+                    </li>
+                    <li class="border-b-2 border-white/20 py-2">
+                        <a href="#" class="text-white font-semibold text-sm hover:text-secondary">
+                            Origen
+                        </a>
+                    </li>
+                    <li class="border-b-2 border-white/20 py-2">
+                        <a href="#" class="text-white font-semibold text-sm hover:text-secondary">
+                            Equipo
+                        </a>
+                    </li>
+                    <li class="border-b border-white/20 py-2">
+                        <a href="#" class="text-white font-semibold text-sm hover:text-secondary">
+                            Materiales
+                        </a>
+                    </li>
+                    <li class="border-b-2 border-white/20 py-2">
+                        <a href="#" class="text-white font-semibold text-sm hover:text-secondary">
+                            Alianzas
+                        </a>
+                    </li>
+                    <li class="border-b-2 border-white/20 py-2">
+                        <a href="#" class="text-white font-semibold text-sm hover:text-secondary">
+                            Contacto
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+            <p class="text-white/50 text-xs font-semibold">
+                © {{ date('Y') }} {{ env('APP_NAME') }}, Derechos Reservados.
+            </p>
         </div>
     </div>
 </header>
