4f289b600b20e0716ae48d7087aba1e11afd0059
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2026-01-14 11:45:26 -0800

    Menu Movil - Fase 1

--- resources/views/components/redesign/website-header.blade.php
+++ resources/views/components/redesign/website-header.blade.php
@@ -1,13 +1,14 @@
 <header class="bg-white shadow relative z-50">
-    <div class="bg-primary w-[50%] h-full absolute top-0 right-0 z-1">
-        <img src="{{ asset('img/svg/header-2-curves.svg') }}" 
+    <div class="bg-primary w-[35%] md:w-[40%] lg:w-[50%] h-full absolute top-0 right-0 z-1">
+        <img src="{{ asset('img/redesign/header-curvas.png') }}" 
             alt="" 
-            class="absolute w-auto" 
-            style="height: 100%; left: -270px; bottom: 0;">
+            class="absolute w-auto left-[-70px] lg:left-[-270px] h-full w-auto bottom-0">
     </div>
-    <div class="container">
+    <div class="container" x-data="{
+        mobileMenu: false,
+    }">
         <div class="flex items-center justify-between">
-            <div class="w-1/3">
+            <div class="w-2/3 lg:w-1/3">
                 <a href="#">
                     <img src="{{ asset('img/centro-luken-logo.svg') }}" 
                         alt="Centro Luken - De Estrategias en Agua y Medio Ambiente"
@@ -15,8 +16,8 @@
                 </a>
             </div>
 
-            <nav class="redesign-nav bg-primary py-8 w-2/3 relative z-2 flex items-center justify-end">
-                <ul class="flex items-center space-x-10">
+            <nav class="lg:flex items-center justify-end redesign-nav bg-primary py-8 w-1/3 lg:w-2/3 relative z-2">
+                <ul class="hidden lg:flex items-center space-x-10">
                     <li>
                         <a href="#" class="text-white font-semibold text-sm hover:text-secondary">
                             Inicio
@@ -64,8 +65,28 @@
                             Alianzas
                         </a>
                     </li>
+                    <li>
+                        <a href="#" class="text-white font-semibold text-sm hover:text-secondary">
+                            Contacto
+                        </a>
+                    </li>
+                </ul>
+
+                <ul class="flex lg:hidden items-center justify-end space-x-4">
+                    <li>
+                        <a href="#" class="text-white font-semibold text-sm hover:text-secondary relative"
+                            style="z-index: 1001;"
+                            @click.prevent="mobileMenu = !mobileMenu">
+                            <i x-show="!mobileMenu" class="lni lni-menu-hamburger-1 text-2xl md:text-3xl"></i>
+                            <i x-show="mobileMenu" class="lni lni-xmark text-2xl md:text-3xl"></i>
+                        </a>
+                    </li>
                 </ul>
             </nav>
+        </div>
+
+        <div class="mobile-menu" :class="{ 'active': mobileMenu }">
+            
         </div>
     </div>
 </header>
