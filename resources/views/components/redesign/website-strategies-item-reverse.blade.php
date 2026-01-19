530a529437486e67855ca4bfd15134a56b4ba853
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2026-01-18 19:24:56 -0800

    Estrategias en versiones de iPad y Moviles

--- resources/views/components/redesign/website-strategies-item-reverse.blade.php
+++ resources/views/components/redesign/website-strategies-item-reverse.blade.php
@@ -1,16 +1,17 @@
-<div class="flex items-stretch min-h-[500px]">
-    <div class="w-1/2">
-        <div class="bg-primary p-10 relative -top-10 z-10">
-            <div class="absolute top-0 -right-[150px] bottom-0 w-[150px] bg-primary"></div>
-            <h3 class="text-white text-2xl font-medium">
+<div class="flex flex-col flex-col-reverse md:flex-row items-stretch min-h-[300px] lg:min-h-[500px]">
+    <div class="w-full md:w-1/2">
+        <div class="bg-primary p-4 lg:p-10 relative top-0 lg:-top-10 z-10">
+            <div class="hidden md:block absolute top-0 -right-[75px] lg:-right-[150px] bottom-0 w-[75px] lg:w-[150px] bg-primary"></div>
+            <h3 class="text-white text-lg lg:text-2xl font-medium">
                 {{ $title }}
             </h3>
         </div>
-        <div class="p-10 wow animate__animated animate__fadeInLeft" data-wow-delay="0.4s">
+        <div class="p-4 lg:p-10 wow animate__animated animate__fadeInLeft" data-wow-delay="0.4s">
             {{ $description }}
         </div>
     </div>
-    <div class="w-1/2 bg-cover bg-center relative overflow-hidden" style="background-image: url('{{ $image }}')">
-        <div class="absolute top-0 left-0 bottom-0 w-[150px] bg-primary bg-opacity-30 wow animate__animated animate__fadeInLeft" data-wow-delay="0.4s"></div>
+    <div class="min-h-[250px] md:min-h-0 w-full md:w-1/2 bg-cover bg-center relative overflow-hidden" style="background-image: url('{{ $image }}')">
+        <div class="hidden md:block absolute top-0 left-0 bottom-0 w-[75px] lg:w-[150px] bg-primary bg-opacity-30 wow animate__animated animate__fadeInLeft" data-wow-delay="0.4s"></div>
+        <div class="block md:hidden absolute left-0 bottom-0 right-0 h-[90px] bg-primary bg-opacity-30 wow animate__animated animate__fadeInUp" data-wow-delay="0.4s"></div>
     </div>
 </div>
--- resources/views/components/redesign/website-strategies-item.blade.php
+++ resources/views/components/redesign/website-strategies-item.blade.php
@@ -1,15 +1,16 @@
-<div class="flex items-stretch min-h-[500px]">
-    <div class="w-1/2 bg-cover bg-center relative overflow-hidden" style="background-image: url('{{ $image }}')">
-        <div class="absolute top-0 right-0 bottom-0 w-[150px] bg-primary bg-opacity-30 wow animate__animated animate__fadeInRight" data-wow-delay="0.4s"></div>
+<div class="flex flex-col md:flex-row items-stretch min-h-[300px] lg:min-h-[500px]">
+    <div class="min-h-[250px] md:min-h-0 w-full md:w-1/2 bg-cover bg-center relative overflow-hidden" style="background-image: url('{{ $image }}')">
+        <div class="hidden md:block absolute top-0 right-0 bottom-0 w-[75px] lg:w-[150px] bg-primary bg-opacity-30 wow animate__animated animate__fadeInRight" data-wow-delay="0.4s"></div>
+        <div class="block md:hidden absolute right-0 bottom-0 h-[90px] w-full bg-primary bg-opacity-30 wow animate__animated animate__fadeInUp" data-wow-delay="0.4s"></div>
     </div>
-    <div class="w-1/2">
-        <div class="bg-primary p-10 relative -top-10 z-10">
-            <div class="absolute top-0 -left-[150px] bottom-0 w-[150px] bg-primary"></div>
-            <h3 class="text-white text-2xl font-medium">
+    <div class="w-full md:w-1/2">
+        <div class="bg-primary p-4 lg:p-10 relative top-0 lg:-top-10 z-10">
+            <div class="hidden md:block absolute top-0 -left-[75px] lg:-left-[150px] bottom-0 w-[75px] lg:w-[150px] bg-primary"></div>
+            <h3 class="text-white text-lg lg:text-2xl font-medium">
                 {{ $title }}
             </h3>
         </div>
-        <div class="p-10 wow animate__animated animate__fadeInRight" data-wow-delay="0.4s">
+        <div class="p-4 lg:p-10 wow animate__animated animate__fadeInRight" data-wow-delay="0.4s">
             {{ $description }}
         </div>
     </div>
