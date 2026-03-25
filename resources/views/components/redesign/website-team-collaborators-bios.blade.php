77f48348fdfc163369bb5caed2317657c0c38d32
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2026-03-24 21:37:38 -0700

    Se agregan las semblanzas del equipo

--- resources/views/components/redesign/website-team-collaborators-bios.blade.php
+++ resources/views/components/redesign/website-team-collaborators-bios.blade.php
@@ -6,7 +6,7 @@
                     Andrea Alonso López
                 </x-slot:name>
                 <x-slot:bio>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, cum cumque. Doloribus cum ad possimus eaque? Natus autem alias hic quia delectus a obcaecati asperiores enim? Voluptatem amet iusto praesentium?
+                    <p>Con más de 10 años de experiencia en la coordinación y supervisión de proyectos, ha participado en el desarrollo de programas, planes, estudios y proyectos binacionales en materia recursos hídricos y desarrollo urbano. Ingeniera química por la UABC con maestría en ciencias con especialidad en Sistemas Ambientales en el área de acentuación de Manejo Sostenible del Agua en por el Instituto Tecnológico y de Estudios Superiores de Monterrey. Ha trabajado en el sector privado y colaborado con la sociedad civil; se desempeñó como servidora pública en el Instituto Metropolitano de Planeación de Tijuana en el área ambiental del 2019 al 2022. Ha enfocado su carrera a integrar los aspectos sociales y ambientales en el desarrollo de estrategias e implementación de herramientas que fomenten un desarrollo urbano sostenible, a través de esfuerzos participativos y multidisciplinarios.</p>
                 </x-slot:bio>
             </x-redesign.website-bio-card>
         </div>
@@ -16,7 +16,7 @@
                     Bertha Alicia Sandoval Franco
                 </x-slot:name>
                 <x-slot:bio>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, cum cumque. Doloribus cum ad possimus eaque? Natus autem alias hic quia delectus a obcaecati asperiores enim? Voluptatem amet iusto praesentium?
+                    <p>Con formación en las áreas de Humanidades y de Administración, cuenta con una vasta experiencia al frente de equipos multidisciplinarios. Fue Coordinadora de Comunicación en El Colegio de la Frontera Norte, y subdirectora de Promoción y Difusión en la Universidad Tecnológica de Tijuana. Previamente tuvo a su cargo el área de Relaciones Públicas de la Cervecería Cuauhtémoc Moctezuma Planta Tecate. En el sistema de Colegios de Estudios Científicos y Tecnológicos de Baja California, fue directora del plantel Cachanilla. Paralelamente, ha sido profesora en la Facultad de Humanidades y Ciencias Sociales en la Universidad Autónoma de Baja California Campus Tijuana. Colabora también en Restauremos El Colorado, A.C. como coordinadora de programas del Consejo Directivo.</p>
                 </x-slot:bio>
             </x-redesign.website-bio-card>
         </div>
@@ -26,7 +26,7 @@
                     Patricia Peterson Villalobos
                 </x-slot:name>
                 <x-slot:bio>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, cum cumque. Doloribus cum ad possimus eaque? Natus autem alias hic quia delectus a obcaecati asperiores enim? Voluptatem amet iusto praesentium?
+                    <p>Tiene más de 30 años de experiencia en la elaboración de proyectos integrales y ha dirigido proyectos de desarrollo urbano, infraestructura y edificación. Arquitecta por la Universidad Autónoma de Baja California (UABC), con Maestría en Desarrollo Urbano por la UIA-Tijuana, y cursos de especialidad en Planeación y Desarrollo de Infraestructura y Desarrollo de Proyectos de Inversión Publico-Privada. Fue secretaria de Desarrollo Territorial, Urbano y Ambiental de Tijuana en el XXIII Ayuntamiento durante 2019-2021. Ha participado en importantes proyectos regionales y en estudios binacionales sobre desalinización, diversos planes maestros para el manejo del agua en Sonora, Zacatecas y Quintana Roo. Fue secretaria de Desarrollo Territorial, Urbano y Ambiental de Tijuana en el XXIII Ayuntamiento durante 2019-2021. Tiene amplia trayectoria gremial, fue presidente del Colegio de Arquitectos de Tijuana (2008-2010), y fundadora de la Sociedad de Urbanismo de la Zona Metropolitana de Tijuana.</p>
                 </x-slot:bio>
             </x-redesign.website-bio-card>
         </div>
--- resources/views/components/redesign/website-team-partners-bios.blade.php
+++ resources/views/components/redesign/website-team-partners-bios.blade.php
@@ -6,7 +6,8 @@
                     Carlos A. de la Parra Rentería
                 </x-slot:name>
                 <x-slot:bio>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, cum cumque. Doloribus cum ad possimus eaque? Natus autem alias hic quia delectus a obcaecati asperiores enim? Voluptatem amet iusto praesentium?
+                    <p class="mb-4">Académico con más de 30 años de estudio en temas de agua y medio ambiente en la frontera México-Estados Unidos. Es ingeniero civil con doctorado en Planificación Ambiental por la Universidad de Michigan. Fue profesor investigador en El Colegio de la Frontera Norte, y ha sido Delegado Federal en Baja California y Representante en la Embajada de México en Washington por la Secretaría de Medio Ambiente y Recursos Naturales (SEMARNAT).</p>
+                    <p>Preside Restauremos el Colorado, una Asociación Civil que opera en Mexicali en la gestión del agua para la restauración de ecosistemas, y es miembro del Consejo Directivo del Banco de Desarrollo de América del Norte (BDAN) como Representante de la Sociedad Civil Mexicana.</p>
                 </x-slot:bio>
             </x-redesign.website-bio-card>
         </div>
@@ -16,7 +17,7 @@
                     Héctor I. Peterson Villalobos
                 </x-slot:name>
                 <x-slot:bio>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, cum cumque. Doloribus cum ad possimus eaque? Natus autem alias hic quia delectus a obcaecati asperiores enim? Voluptatem amet iusto praesentium?
+                    <p>Ingeniero Civil formado en el Instituto Tecnológico y de Estudios Superiores de Monterrey (ITESM); realizó la Maestría en Desarrollo Urbano con especialidad en Sistemas de Información Geográfica en la Universidad Iberoamericana (UIA), así como diplomados de Desarrollo de Proyectos de Inversión Publico-Privada, Desarrollo Gerencial, Desarrollo Urbano y Contabilidad Administrativa. Con más de 30 años de experiencia en el desarrollo de proyectos integrales y de infraestructura hidráulica de gran alcance, formó la empresa Libra Ingenieros Civiles S. A. de C. V., desde la cual ha participado en importantes proyectos regionales destacando, entre ellos, los primeros de desalinización para uso público urbano en el país, así como en alianzas para estudios binacionales con Estados Unidos para infraestructura hídrica. También ha participado en diversos planes y proyectos para el manejo del agua en Baja California, Baja California Sur, Sonora, Zacatecas y Quintana Roo. Ha trabajado desde la sociedad civil para impulsar la inversión en infraestructura hídrica desde un enfoque productivo, además de ser impulsor de proyectos bajo la modalidad de asociación público-privada en la región.</p>
                 </x-slot:bio>
             </x-redesign.website-bio-card>
         </div>
@@ -26,7 +27,8 @@
                     Héctor M. Arias Rojo
                 </x-slot:name>
                 <x-slot:bio>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, cum cumque. Doloribus cum ad possimus eaque? Natus autem alias hic quia delectus a obcaecati asperiores enim? Voluptatem amet iusto praesentium?
+                    <p>Es químico con maestría en Física de Suelos y doctorado en Manejo de Cuencas Hidrográficas. Ha sido director de Sustentabilidad y Fortalecimiento, así como líder del proyecto Estimulación de Lluvia para mitigar la sequía en las Zonas Áridas dentro de la Comisión Nacional de las Zonas Áridas. También fue director del Instituto para la Gestión Integral de Cuencas Hidrográficas, donde se estudia y gestionan los temas de Drenaje Parcelario, Protección Ambiental, y Manejo de Cuencas Hidrográficas con enfoque a productores agrícolas, comunidades indígenas, agencias internacionales y nacionales, organizaciones gubernamentales y no gubernamentales. Ha colaborado con la Comisión Nacional del Agua, la Comisión de Áreas Naturales Protegidas, la Comisión Nacional Forestal, la Comisión Internacional de Límites y Aguas, la Secretaría de Relaciones Exteriores, la empresa minera Peñoles-México, el Natural Heritage Institute-San Francisco, y los gobiernos de Chihuahua y Sonora, entre otras.</p>
+                    <p>Es Investigador Nacional Nivel II dentro del Sistema Nacional de Investigadores, con más de 70 publicaciones sobre temas de Manejo de Cuencas Hidrográficas, Conservación de Suelos y Recursos Naturales. También es coautor del libro “Alternative Futures for Changing Landscapes. The Upper San Pedro River Basin in Arizona and Sonora”, editado por Harvard Univerity Press, esfuerzo multi-institucional con las universidades de Harvard, Arizona e IMADES en el río San Pedro, Sonora, México, y Arizona, Estados Unidos.</p>
                 </x-slot:bio>
             </x-redesign.website-bio-card>
         </div>
@@ -36,7 +38,7 @@
                     Martín A. Delgado Saldívar
                 </x-slot:name>
                 <x-slot:bio>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, cum cumque. Doloribus cum ad possimus eaque? Natus autem alias hic quia delectus a obcaecati asperiores enim? Voluptatem amet iusto praesentium?
+                    <p>Experiencia en presupuestos gubernamentales basados en evaluación por resultados y planeación participativa, evaluación de proyectos, análisis de costo y beneficio y valuación de empresas. Ingeniero Industrial (ITSON) con Maestría en Ingeniería Industrial (Texas A&M University), Diplomado en Alta Dirección de Empresas (IPADE) y Doctor en Desarrollo Regional (CIAD). Actualmente participa como asesor externo del H. Ayuntamiento de Hermosillo y el H. Ayuntamiento de Yécora. Ha realizado trabajos de consultoría para las empresas o instituciones como SICTUHSA, SOLISON, GOODNESS, MERCAMAR, CIAD-CONAGUA, PRODEREM, WWF, CILA, Consejo de Educación de Puebla, US-AID, CONAGUA, PEÑOLES, CONAFOR, CEA-Sonora, y la OMM-ONU entre otras. Fue Jefe del Departamento de Administración en la Universidad de Sonora e imparte materias en las Maestría en Administración y en Comercio Exterior y Aduanas además de las Licenciaturas del Finanzas y Negocios Internacionales. Fue Director General del Instituto del Medio Ambiente y el Desarrollo Sustentable del Estado de Sonora, del Centro de Investigación y Desarrollo de los Recursos Naturales de Sonora y del Centro Ecológico de Sonora, de Blazer Internacional (Cananea-Naco-Besbee), Fábrica de Tubos Vibroprensados S.A, FATUVISA (Hermosillo) y Poliestireno de Sonora (Ciudad Obregón).</p>
                 </x-slot:bio>
             </x-redesign.website-bio-card>
         </div>
@@ -46,7 +48,7 @@
                     Roberto Fernando Samón Castelo (QEPD)
                 </x-slot:name>
                 <x-slot:bio>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, cum cumque. Doloribus cum ad possimus eaque? Natus autem alias hic quia delectus a obcaecati asperiores enim? Voluptatem amet iusto praesentium?
+                    <p>Asesor de instituciones públicas y privadas en materia de gestión internacional del agua y manejo de recursos hídricos. Egresado de la Universidad de Arizona como ingeniero agrónomo, con maestría en economía agrícola y en proceso de titulación de estudios doctorales en Administración de Recursos Hidráulicos de la misma universidad. Se desempeñó como Comisionado mexicano ante la Comisión Internacional de Límites y Aguas entre México y Estados Unidos (CILA) desde abril de 2009 hasta mayo de 2020. Como líder de negociación del grupo mexicano, logró la firma de 11 acuerdos binacionales (Actas) con Estados Unidos, que son vinculantes y de cumplimiento obligatorio para ambos países. Ha sido funcionario en la Comisión Nacional del Agua como Gerente de la Región II Noroeste (2002 - 2006) y director general del Organismo de Cuenca Noroeste (2006 - 2009) y director de Planeación y Proyectos Especiales en el Centro de Investigaciones y Desarrollo de los Recursos Naturales del Estado de Sonora (CIDESON).</p>
                 </x-slot:bio>
             </x-redesign.website-bio-card>
         </div>
