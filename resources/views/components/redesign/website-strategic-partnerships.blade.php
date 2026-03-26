9cd12af5d435f82bd5278ab990a2587b75577b37
Jonathan Velazquez <e.jonathan.velazquez@gmail.com>
2026-03-25 21:45:54 -0700

    Se realizan las actualizaciones finales para el deployment en la raiz principal

--- resources/views/components/redesign/website-strategic-partnerships.blade.php
+++ resources/views/components/redesign/website-strategic-partnerships.blade.php
@@ -9,21 +9,31 @@
             </p>
         </div>
 
-        <div class="flex flex-col md:flex-row space-y-5 md:space-y-0 items-center justify-center space-x-0 md:space-x-5 relative left-0 md:-left-5">
+        <div class="flex flex-col md:flex-row space-y-5 md:space-y-0 items-center justify-center space-x-0 md:space-x-5 relative left-0 md:-left-5" style="z-index: 100;">
             <div class="w-9/12 md:w-1/5 flex items-center justify-center wow animate__animated animate__fadeInUp">
-                <img src="{{ asset('redesign/img/partnerships/alianza-revive.jpg') }}" class="max-h-[80px] object-contain" alt="Alianza Revive - Centro Luken">
+                <a href="https://www.reviveelrio.org/" target="_blank" rel="noopener noreferrer">
+                    <img src="{{ asset('redesign/img/partnerships/alianza-revive.jpg') }}" class="max-h-[80px] object-contain" alt="Alianza Revive - Centro Luken">
+                </a>
             </div>
             <div class="w-9/12 md:w-1/5 flex items-center justify-center wow animate__animated animate__fadeInUp" data-wow-delay="0.2s">
-                <img src="{{ asset('redesign/img/partnerships/the-walton-family.jpg') }}" class="max-h-[80px] object-contain" alt="The Walton Family - Centro Luken">
+                <a href="https://www.waltonfamilyfoundation.org/" target="_blank" rel="noopener noreferrer">
+                    <img src="{{ asset('redesign/img/partnerships/the-walton-family.jpg') }}" class="max-h-[80px] object-contain" alt="The Walton Family - Centro Luken">
+                </a>
             </div>
             <div class="w-9/12 md:w-1/5 flex items-center justify-center wow animate__animated animate__fadeInUp" data-wow-delay="0.4s">
-                <img src="{{ asset('redesign/img/partnerships/bonneville.jpg') }}" class="max-h-[80px] object-contain" alt="Bonneville - Centro Luken">
+                <a href="https://www.b-e-f.org/" target="_blank" rel="noopener noreferrer">
+                    <img src="{{ asset('redesign/img/partnerships/bonneville.jpg') }}" class="max-h-[80px] object-contain" alt="Bonneville - Centro Luken">
+                </a>
             </div>
             <div class="w-9/12 md:w-1/5 flex items-center justify-center wow animate__animated animate__fadeInUp" data-wow-delay="0.6s">
-                <img src="{{ asset('redesign/img/partnerships/blue-commons.jpg') }}" class="max-h-[80px] object-contain" alt="Blue Commons - Centro Luken">
+                <a href="https://bluecommons.org/rebeaver/" target="_blank" rel="noopener noreferrer">
+                    <img src="{{ asset('redesign/img/partnerships/blue-commons.jpg') }}" class="max-h-[80px] object-contain" alt="Blue Commons - Centro Luken">
+                </a>
             </div>
             <div class="w-9/12 md:w-1/5 flex items-center justify-center wow animate__animated animate__fadeInUp" data-wow-delay="0.8s">
-                <img src="{{ asset('redesign/img/partnerships/rio-arronte.jpg') }}" class="max-h-[80px] object-contain" alt="Rio Arronte - Centro Luken">
+                <a href="https://fundaciongonzalorioarronte.org/" target="_blank" rel="noopener noreferrer">
+                    <img src="{{ asset('redesign/img/partnerships/rio-arronte.jpg') }}" class="max-h-[80px] object-contain" alt="Rio Arronte - Centro Luken">
+                </a>
             </div>
         </div>
     </div>
