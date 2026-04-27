3452e21c5261c42337d2bf6ff8da96cd69837a63
Jonathan Velazquez <ejonathanv.dev@gmail.com>
2026-04-27 14:48:39 -0700

    Se corrigen las vistas de topics para utilizar el layout nuevo

--- resources/views/dashboard/topics/create.blade.php
+++ resources/views/dashboard/topics/create.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-redesign.website-app-layout>
     <section class="py-16">
         <div class="container">
             <div class="w-8/12 mx-auto">
@@ -171,4 +171,4 @@
     <section class="section_divider"></section>
     <section class="section_divider"></section>
 
-</x-app-layout>
+</x-redesign.website-app-layout>
