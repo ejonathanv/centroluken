7b597967f0c84dc552e7afeb04ceef8f1fed9dda
Jonathan Velazquez <ejonathanv.dev@gmail.com>
2026-07-10 08:56:47 -0700

    Modificación en titulo de proyectos

--- lang/es/2026/materials.php
+++ lang/es/2026/materials.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'studies_quote' => 'Proyectos y líneas temáticos de trabajo',
+    'studies_quote' => 'Proyectos y campos temáticos de trabajo',
     'studies_intro' => 'Nuestros campos temáticos de trabajo están relacionados con la sostenibilidad ambiental, por ejemplo, gobernanza del agua; tratamiento y reutilización de aguas; restauración de ecosistemas; agricultura regenerativa; energía solar y agrivoltáica; manejo de suelos y cambio climático.',
     'resources' => [
         'white_quote' => 'Conoce nuestros análisis y discusiones más recientes sobre los temas relevantes del sector; y su cobertura en medios de comunicación',
