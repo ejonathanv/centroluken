21bc22c936bdb3eee12156db93778b2040b77425
Jonathan Velazquez <ejonathanv.dev@gmail.com>
2026-07-27 13:39:57 -0700

    Actualización de texto en ingles de seccion de proyectos.

--- lang/en/2026/materials.php
+++ lang/en/2026/materials.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'studies_quote' => 'Projects to Date',
+    'studies_quote' => 'Projects and Lines of Work',
+    'studies_intro' => 'Our thematic fields of work are related to environmental sustainability, such as water governance, water treatment and reuse, ecosystem restoration, regenerative agriculture, solar energy and agrivoltaics, soil management, and climate change.',
     'resources' => [
         'white_quote' => 'Stay informed with our latest industry analyses, discussions, and media highlights',
         'secondary_quote' => 'Resources',
